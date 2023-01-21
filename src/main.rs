f9ea4ee3f5486dfc0d6b10ba74f7a52f92e1ba08
Kazuki Chigita <kazuki.chigichan@gmail.com>
2023-01-22 00:49:45 +0900

    Solve B

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,12 @@
-use std::mem;
-
 use proconio::*;
 
 fn main() {
     input! {
         n: usize,
-        p: usize,
-        q: usize,
-        r: usize,
-        s: usize,
-        mut a: [usize; n],
+        s: String,
     }
-    for i in (p-1)..q {
-        a.swap(i,i+r-p);
-    }
-    for i in 0..n {
-        print!("{} ", a[i]);
-    }
-    println!("");
+    let from = "na";
+    let to = "nya";
+    let result = s.replace(&from, to);
+    println!("{}", result);
 }
